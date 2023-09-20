e31cc939e051aeac5ecdd69ac263030313a8cc5f
KiskinAlexander <145544502+KiskinAlexander@users.noreply.github.com>
2023-09-20 11:42:14 +0300

    Add files via upload

---
+++ 1 Практика - Черепаха/Упражнение №3 квадрат.py
@@ -0,0 +1,14 @@
+# Задание по пути: http://judge.mipt.ru/mipt_cs_on_python3/labs/lab1.html
+# Необходимо нарисовать квадрат: http://judge.mipt.ru/mipt_cs_on_python3/images/lab1/rectangle.gif
+import turtle
+
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
