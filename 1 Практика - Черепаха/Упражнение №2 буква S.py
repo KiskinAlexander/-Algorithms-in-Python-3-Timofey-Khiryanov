742afe078089da2ca9ef9dddd453e574cad85e6f
KiskinAlexander <145544502+KiskinAlexander@users.noreply.github.com>
2023-09-20 11:37:21 +0300

    Add files via upload

---
+++ 1 Практика - Черепаха/Упражнение №2 буква S.py
@@ -0,0 +1,13 @@
+# Задание по пути: http://judge.mipt.ru/mipt_cs_on_python3/labs/lab1.html
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
