310c1611614e2ee076fa48747112e2210787cffb
KiskinAlexander <145544502+KiskinAlexander@users.noreply.github.com>
2023-09-22 13:39:54 +0300

    Update Упражнение 3 - Наименьшая стоимость достижения клетки.py

--- 11 Практика - Динамическое программирование/Упражнение 3 - Наименьшая стоимость достижения клетки.py
+++ 11 Практика - Динамическое программирование/Упражнение 3 - Наименьшая стоимость достижения клетки.py
@@ -1,14 +1,14 @@
 # Напишите функцию calculate_min_cost(n, price) вычисления наименьшей стоимость достижения клетки n из клетки 1
 def count_min_cost(n, price):
     # В точку 1 можно попать только из точки 0
-    c = [0] * n
+    c = [0] * (n+1)
     c[0] = price[0]
     c[1] = c[0] + price[1]
-    for i in range(2, n):
+    for i in range(2, n+1):
         c[i] = price[i] + min(c[i - 1], c[i - 2])
     return c
 
 f = int(input('Введите n:'))
-s = input('Введите последовательность стоимостей клеток:')
+s = input('Введите последовательность стоимостей клеток (n+1) штук:')
 values = [int(x) for x in s.split(' ')]
 print(count_min_cost(f, values))
